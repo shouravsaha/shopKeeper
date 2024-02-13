8aac758378dfc31ca49edf57ab18e6eaa6d9d4bc
Shourav Saha <shourav939@gmail.com>
2024-02-13 07:04:02 +0600

    working on blade

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -13,24 +13,10 @@
     </head>
     <body class="sb-nav-fixed">
 
-
-
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="{{ url('index') }}">Shop Keeper</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <div class="d-none d-md-inline-block form-inline ms-auto me-auto me-md-3 my-2 my-md-0">
-                {{-- <h1 class="text-light text-center">Shop-Keeper Management System</h1> --}}
-            </div>
-            <!-- Navbar-->
-        </nav>
-
-
+        @yield('navbar')
 
         <div id="layoutSidenav">
+
             <div id="layoutSidenav_nav">
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
@@ -72,19 +58,18 @@
                                 DataTable Example
                             </div>
                             <div class="card-body">
-                                <table id="" class="table">
+                                <table /*id="datatablesSimple"*/ class="table table-bordered text-center">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+                                            <th>Product Name</th>
+                                            <th>Description</th>
+                                            <th>Price</th>
+                                            <th>Stock</th>
+                                            <th>Action</th>
 
                                         </tr>
                                     </thead>
-                                    <tfoot>
+                                    {{-- <tfoot>
                                         <tr>
                                             <th>Name</th>
                                             <th>Position</th>
@@ -93,63 +78,17 @@
                                             <th>Start date</th>
                                             <th>Salary</th>
                                         </tr>
-                                    </tfoot>
+                                    </tfoot> --}}
                                     <tbody>
                                         <tr>
                                             <td>Tiger Nixon</td>
                                             <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Garrett Winters</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>63</td>
-                                            <td>2011/07/25</td>
-                                            <td>$170,750</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Ashton Cox</td>
-                                            <td>Junior Technical Author</td>
-                                            <td>San Francisco</td>
-                                            <td>66</td>
-                                            <td>2009/01/12</td>
-                                            <td>$86,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Cedric Kelly</td>
-                                            <td>Senior Javascript Developer</td>
-                                            <td>Edinburgh</td>
-                                            <td>22</td>
-                                            <td>2012/03/29</td>
-                                            <td>$433,060</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Airi Satou</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>33</td>
-                                            <td>2008/11/28</td>
-                                            <td>$162,700</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Brielle Williamson</td>
-                                            <td>Integration Specialist</td>
-                                            <td>New York</td>
-                                            <td>61</td>
-                                            <td>2012/12/02</td>
-                                            <td>$372,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Herrod Chandler</td>
-                                            <td>Sales Assistant</td>
-                                            <td>San Francisco</td>
-                                            <td>59</td>
-                                            <td>2012/08/06</td>
-                                            <td>$137,500</td>
+                                            <td>$100</td>
+                                            <td>10</td>
+                                            <td>
+                                                <i class="fas fa-edit edit"></i>
+                                                <i class="fa-solid fa-trash delete"></i>
+                                            </td>
                                         </tr>
 
                                     </tbody>
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.index')
+@section('data')
+<h1>hi</h1>
+@endsection
